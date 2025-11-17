5622e82a833c5a7359d35bc0cc36bbb5c4cce7b6
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2025-11-17 14:36:09 +0300

    First

---
+++ src/test/java/com/ramazanayyildiz/EMS/EmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramazanayyildiz.EMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
