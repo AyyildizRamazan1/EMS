907163e2a6affd495083cc474962b0654bc0ce09
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2026-01-04 15:38:29 +0300

    validasyon hataları için GlobalExceptionHandler iyileştirmesi yapıldı

--- src/main/java/com/ramazanayyildiz/EMS/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ramazanayyildiz/EMS/exception/GlobalExceptionHandler.java
@@ -2,11 +2,14 @@ package com.ramazanayyildiz.EMS.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -21,5 +24,18 @@ public class GlobalExceptionHandler {
         body.put("path", request.getDescription(false).substring(4));
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST); // 400 Bad Request
     }
 }
